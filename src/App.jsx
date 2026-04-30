1020169eb4b6fac17e3924b1f981199e9af52730
IAGO CARNEIRO DE MEDEIROS <iago_medeiros@estudante.sesisenai.org.br>
2026-04-30 11:39:42 -0300

    login, cadastro e usuario

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
 import { useState } from 'react'
 import './App.css'
+import Login from './paginas/Login'
+import Cadastro from './paginas/Cadastro'
+import Usuario from './paginas/Usuario'
 
 function App() {
+
+  const [tela, setTela] = useState('login')
+
   const renderizarTela = () => {
-    //navegação
+    if (tela === 'login') {
+      return <Login/>
+    } else if (tela === 'cadastro') {
+      return <Cadastro/>
+    } else if (tela === 'usuario') {
+      return <Usuario/>
+    }
+  }
+
+  const trocarTela = (pagina) => {
+    setTela(pagina)
   }
   
   return (
     <>
       <section id="center">
-
+        <h1>giulia é legal demais</h1>
+        <button onClick={ () => trocarTela('login')}>login</button>
+        <button onClick={ () => trocarTela('cadastro')}>cadastro</button>
+        <button onClick={ () => trocarTela('usuario')}>usuario</button>
+        {renderizarTela()}
       </section>
 
     </>
